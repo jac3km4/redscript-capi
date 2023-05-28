ad955bc7d7cf6168eb5bf140845db7bfb9b04ded
jekky <11986158+jac3km4@users.noreply.github.com>
2023-05-28 18:44:22 +0100

    Basic error checking

--- examples/decompile.c
+++ examples/decompile.c
@@ -2,7 +2,16 @@
 
 void main() {
     struct ScriptBundle* bundle = bundle_load("C:\\Games\\Cyberpunk 2077\\r6\\cache\\final.redscripts.bk");
+    if (bundle == NULL) {
+        printf("Failed to load bundle\n");
+        return;
+    }
     struct Decompilation* dec = decompile_global(bundle, "LogStats;script_ref<String>");
+    if (dec == NULL) {
+        printf("Failed to decompile\n");
+        return;
+    }
+
     printf("Decompiled function:\n%s\n", decompilation_code(dec));
 
     decompilation_free(dec);
